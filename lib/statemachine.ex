6dc8a3848ee124afe19edc2c29820d979349eea4
Klaus Alfert <klaus.alfert@gmail.com>
2017-12-27 17:29:32 +0100

    Remove old non-working code and rename fishcake’s version.

--- lib/statemachine.ex
+++ lib/statemachine.ex
@@ -110,78 +110,39 @@ defmodule Statemachine do
   end
 
 
-  ###########################
-  # unfolding
-  def gen_cmd(initial_state, cmd_gen, next_fun) do
-    StreamData.sized(fn
-      0 -> nil
-      n -> gen_cmd(n, initial_state, cmd_gen, next_fun)
-    end)
-  end
-
-  def gen_cmd(0, _acc, _cmd_gen, _next_fun), do: StreamData.constant([])
-  def gen_cmd(size, acc, cmd_gen, next_fun) do
-    cmd_gen.(acc)
-    |> StreamData.bind(fn cmd ->
-      new_acc = {acc, cmd}
-      new_state = next_fun.(cmd, acc)
-      StreamData.frequency([
-        {1, StreamData.constant([])},
-        {size, StreamData.fixed_list([StreamData.constant(new_acc)
-                    | gen_cmd(size - 1, new_state, cmd_gen, next_fun)])}
-        ])
-    end)
-  end
-
-  #######################
-  # `gen_list` does not work, since it builds a direct list.
-
-  def gen_list(gen) do
-    StreamData.sized(fn
-        0 -> nil
-        n -> gen_list(n, gen)
-      end)
-  end
-  def gen_list(0, _), do: StreamData.constant([])
-  def gen_list(n, gen) do
-    gen.()
-    |> StreamData.bind(fn value -> [value | gen_list(n-1, gen)] end)
-    |> StreamData.map(fn {:call, _, c, _} -> c end)
-  end
-
   ##################################
   # @fishcake's unfold does not shrink properly.
 
-  @spec unfold(acc, (acc -> StreamData.t(val)), (val, acc -> {other_val, acc})) ::
+  @spec fishcake_unfold(acc, (acc -> StreamData.t(val)), (val, acc -> {other_val, acc})) ::
         StreamData.t(other_val) when acc: var, val: var, other_val: var
-  def unfold(acc, value_fun, next_fun) do
+  def fishcake_unfold(acc, value_fun, next_fun) do
     StreamData.sized(fn
       0 ->
         StreamData.constant(nil)
       size ->
         acc
-        |> unfold(value_fun, next_fun, size)
+        |> fishcake_unfold(value_fun, next_fun, size)
         # |> StreamData.map(&{:__block__, [], &1})
     end)
   end
 
-  defp unfold(acc, value_fun, next, size) do
+  defp fishcake_unfold(acc, value_fun, next, size) do
     acc
     |> value_fun.()
-    |> StreamData.bind(&unfold_next(&1, acc, value_fun, next, size))
+    |> StreamData.bind(&fishcake_unfold_next(&1, acc, value_fun, next, size))
   end
 
-  defp unfold_next(value, acc, value_fun, next, size) do
+  defp fishcake_unfold_next(value, acc, value_fun, next, size) do
     {quoted, acc} = next.(value, acc)
     acc
-    |> unfold_tail(value_fun, next, size-1)
+    |> fishcake_unfold_tail(value_fun, next, size-1)
     |> StreamData.map(&[quoted | &1])
   end
 
-  defp unfold_tail(acc, value_fun, next, size) do
+  defp fishcake_unfold_tail(acc, value_fun, next, size) do
     StreamData.frequency([
       {1, StreamData.constant([])},
-      {size, unfold(acc, value_fun, next, size)}
+      {size, fishcake_unfold(acc, value_fun, next, size)}
     ])
   end
 
