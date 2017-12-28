2448190311c42819ba27165e7a67774c376b05b4
Klaus Alfert <klaus.alfert@gmail.com>
2017-12-28 07:38:22 +0100

    Its fishcakez not fishcake

--- lib/statemachine.ex
+++ lib/statemachine.ex
@@ -1,7 +1,7 @@
 defmodule Statemachine do
   @moduledoc """
   Property-based testing or better data generation with a state machine in mind.
-  The `unfold`-algorithm is borrowed from @fishcake.
+  The `fishcakez_unfold`-algorithm is borrowed from @fishcakez.
   """
 
   ###############
@@ -149,36 +149,36 @@ defmodule Statemachine do
   ##################################
   # @fishcake's unfold does not shrink properly.
 
-  @spec fishcake_unfold(acc, (acc -> StreamData.t(val)), (val, acc -> {other_val, acc})) ::
+  @spec fishcakez_unfold(acc, (acc -> StreamData.t(val)), (val, acc -> {other_val, acc})) ::
         StreamData.t(other_val) when acc: var, val: var, other_val: var
-  def fishcake_unfold(acc, value_fun, next_fun) do
+  def fishcakez_unfold(acc, value_fun, next_fun) do
     StreamData.sized(fn
       0 ->
         StreamData.constant(nil)
       size ->
         acc
-        |> fishcake_unfold(value_fun, next_fun, size)
+        |> fishcakez_unfold(value_fun, next_fun, size)
         # |> StreamData.map(&{:__block__, [], &1})
     end)
   end
 
-  defp fishcake_unfold(acc, value_fun, next, size) do
+  defp fishcakez_unfold(acc, value_fun, next, size) do
     acc
     |> value_fun.()
-    |> StreamData.bind(&fishcake_unfold_next(&1, acc, value_fun, next, size))
+    |> StreamData.bind(&fishcakez_unfold_next(&1, acc, value_fun, next, size))
   end
 
-  defp fishcake_unfold_next(value, acc, value_fun, next, size) do
+  defp fishcakez_unfold_next(value, acc, value_fun, next, size) do
     {quoted, acc} = next.(value, acc)
     acc
-    |> fishcake_unfold_tail(value_fun, next, size-1)
+    |> fishcakez_unfold_tail(value_fun, next, size-1)
     |> StreamData.map(&[quoted | &1])
   end
 
-  defp fishcake_unfold_tail(acc, value_fun, next, size) do
+  defp fishcakez_unfold_tail(acc, value_fun, next, size) do
     StreamData.frequency([
       {1, StreamData.constant([])},
-      {size, fishcake_unfold(acc, value_fun, next, size)}
+      {size, fishcakez_unfold(acc, value_fun, next, size)}
     ])
   end
 
