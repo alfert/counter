a11c03c725df015951ae7dca51fce7c5beb9728e
Klaus Alfert <klaus.alfert@gmail.com>
2018-01-28 22:45:39 +0100

    Adopt the tests to the new structure

--- test/monad_test.exs
+++ test/monad_test.exs
@@ -7,15 +7,16 @@ defmodule Counter.PropCheck.Monads.Test do
 
     test "create a default generator struct which is not a function" do
       def_gen = Generator.new()
-      assert :gen_fun_t == def_gen.gen_fun
+      # 5 is an arbitrary integer value
+      assert nil == def_gen.run_gen.(5)
     end
 
     test "create a paraemterized generator struct" do
       seed = 5
       size = 10
-      def_gen = Generator.new(fn my_seed, my_size -> my_seed*my_size end)
-      assert is_function(def_gen.gen_fun, 2)
-      assert def_gen.gen_fun.(seed, size) == seed*size
+      def_gen = Generator.new(fn my_seed -> my_seed*size end)
+      assert is_function(def_gen.run_gen, 1)
+      assert def_gen.run_gen.(seed) == seed*size
     end
 
   end
