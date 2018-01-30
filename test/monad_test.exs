c1df2d3c657177c6abb33cffa9bd68efdb16af7a
Klaus Alfert <klaus.alfert@gmail.com>
2018-01-30 07:19:41 +0100

    Add a test for a functor

--- test/monad_test.exs
+++ test/monad_test.exs
@@ -3,6 +3,7 @@ defmodule Counter.PropCheck.Monads.Test do
   use ExUnit.Case
 
   alias Counter.PropCheck.Generator
+  alias Witchcraft.Functor
   describe "Generators are Monads" do
 
     test "create a default generator struct" do
@@ -17,6 +18,15 @@ defmodule Counter.PropCheck.Monads.Test do
       def_gen = Generator.new(fn my_seed -> my_seed*size end)
       assert is_function(def_gen.run_gen, 1)
       assert def_gen.run_gen.(seed) == seed*size
+    end
+
+    test "a generator functor provides a map" do
+      gen = Generator.new(fn x -> 2*x end)
+      mapper = fn x -> x + 1 end
+      gen2 = gen |> Functor.map(mapper)
+      for i <- 1..100 do
+        assert gen2.run_gen.(i) == mapper.(2*i)
+      end
     end
 
   end
