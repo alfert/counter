d60636ea27ca77b6fd6dcb480b3b2b285a75701e
Klaus Alfert <klaus.alfert@gmail.com>
2018-03-31 16:24:53 +0200

    Remove all aspects of `fail` and focus on counting. `fail` is in its own test module

--- test/counter_stream_test.exs
+++ test/counter_stream_test.exs
@@ -10,29 +10,9 @@ defmodule CounterStreamTest do
   require Logger
   import ExUnit.CaptureLog
 
-  property "find the fail command" do
-    assert_raise ExUnit.AssertionError, fn ->
-      check all cmds <- StreamData.list_of(command(:one)) do
-        assert Enum.all?(cmds, &no_failed_call?/1)
-      end
-    end
-  end
-
-  property "ensure that fail is finally generated" do
-    ex = assert_raise ExUnit.AssertionError, fn ->
-      check all cmds <- SM.generate_commands(__MODULE__) do
-        if Enum.any?(cmds, &failed_call?/1) do
-          capture_log(fn -> Logger.error "cmds = #{inspect cmds}" end)
-        end
-        assert Enum.all?(cmds, &no_failed_call?/1)
-      end
-    end
-    assert ex.message =~ ":fail"
-  end
-
-  property "check the counter command execution" do
+  property "check the counter values command execution" do
     check all cmds <- SM.generate_commands(__MODULE__) do
-      IO.puts "Commands are: #{inspect cmds}"
+      capture_log(fn -> Logger.debug "Commands are: #{inspect cmds}" end)
       Process.flag(:trap_exit, true)
       pid = case Counter.start_link() do
         {:ok, c_pid}  -> c_pid
@@ -41,13 +21,10 @@ defmodule CounterStreamTest do
       events = SM.run_commands(__MODULE__, cmds)
       :ok = GenServer.stop(pid, :normal)
       wait_for_stop(pid)
+      capture_log(fn -> Logger.error "Events = #{inspect events}" end)
       assert events.result == :ok
     end
   end
-
-  def no_failed_call?(c), do: not failed_call?(c)
-  def failed_call?({_s, call}), do: failed_call?(call)
-  def failed_call?({:call, _, c, _}), do: c == :fail
 
   def wait_for_stop(pid) do
     ref = Process.monitor(pid)
@@ -59,34 +36,16 @@ defmodule CounterStreamTest do
   def initial_state(), do: :init
 
   # all commands are allowed, the failure is detected in next state
-  def command(:one) do
-    StreamData.frequency([
-      {10, {:call, Counter, :clear, StreamData.constant([])}},
-      {30, {:call, Counter, :inc, StreamData.constant([])}},
-      {10, {:call, Counter, :get, StreamData.constant([])}},
-      # {1, {:call, Counter, :fail, StreamData.constant([])}}
-    ])
-  end
-  def command(:init) do
-    StreamData.frequency([
-      {10, {:call, Counter, :clear, StreamData.constant([])}},
-      {30, {:call, Counter, :inc, StreamData.constant([])}},
-      # {10, {:call, Counter, :get, StreamData.constant([])}},
-      # {1, {:call, Counter, :fail, StreamData.constant([])}}
-      ])
-  end
   def command(_) do
     StreamData.frequency([
       {10, {:call, Counter, :clear, StreamData.constant([])}},
       {30, {:call, Counter, :inc, StreamData.constant([])}},
-      {10, {:call, Counter, :get, StreamData.constant([])}},
-      # {1, {:call, Counter, :fail, StreamData.constant([])}}
+      {10, {:call, Counter, :get, StreamData.constant([])}}
       ])
   end
   @type state_type :: any
   @type call_type :: {:call, atom, atom, list(any)}
-  @spec next_state(call :: call_type, state :: state_type)
-      :: {Macro.t, state_type}
+  @spec next_state(call :: call_type, state :: state_type) :: {atom, state_type}
   def next_state(c = {:call, _,:inc, _}, :init), do:  {the_fun(c), :zero}
   def next_state(c = {:call, _,:clear, _}, :init), do:  {the_fun(c), :zero}
   def next_state(c = {:call, _,:clear, _}, :zero), do:  {the_fun(c), :zero}
@@ -94,9 +53,8 @@ defmodule CounterStreamTest do
   def next_state(c = {:call, _,:inc, _}, :one), do:  {the_fun(c), :one}
   def next_state(c = {:call, _,:clear, _}, :one), do:  {the_fun(c), :zero}
   def next_state(c = {:call, _,:get, _}, state), do:  {the_fun(c), state}
-  def next_state(c = {:call, _,:fail, _}, state), do:  {the_fun(c), state}
 
-  def precondition(:init, {:call, _, :get, _}), do: false
+  # def precondition(:init, {:call, _, :get, _}), do: false
   def precondition(_, _), do: true
 
   @doc "The expected outcome. Only called after executing the command"
@@ -109,11 +67,7 @@ defmodule CounterStreamTest do
   def postcondition(:inc, {:call, _,:clear, _}, _result), do: true
   def postcondition(:zero, {:call, _,:get, _}, 0), do: true
   def postcondition(:one,  {:call, _,:get, _}, result), do: result > 0
-  def postcondition(:one, {:call, _,:inc, _}, result) do
-    # generate a consistent failure
-    result != 6
-    # true
-  end
+  def postcondition(:one, {:call, _,:inc, _}, result), do: result != 6
   def postcondition(:one, {:call, _,:clear, _}, _result), do: true
   def postcondition(_old_state, {:call, _m, _f, _a}, _result) do
     false
